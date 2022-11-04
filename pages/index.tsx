9e01028e3324b44badd6d652af578c256e9b4574
K41z3n <tplermit@gmail.com>
2022-11-04 16:13:34 -0400

    components products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import { Card, CardActionArea, CardMedia, Grid, Typography } from "@mui/material"
+import { Typography } from "@mui/material"
 import { NextPage } from "next"
 import ShopLayout from "../components/layouts/ShopLayout"
 import { initialData } from "../database/products"
 
+import { ProductList } from "../components/products"
 
 const Home: NextPage = () => {
   return (
@@ -11,19 +12,7 @@ const Home: NextPage = () => {
       <Typography variant="h1" component='h1'>Tienda</Typography>
       <Typography variant="h2" sx={{ mb: 1 }} component='h2'>Todos los productos</Typography>
 
-      <Grid container spacing={4}>
-        {
-          initialData.products.map(prod => (
-            <Grid item sm={4} key={prod.slug}>
-              <Card>
-                <CardActionArea>
-                  <CardMedia component='img' image={`products/${prod.images[0]}`} alt={prod.title} />
-                </CardActionArea>
-              </Card>
-            </Grid>
-          ))
-        }
-      </Grid>
+      <ProductList products={initialData.products as any} />
 
     </ShopLayout>
   )
