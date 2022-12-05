c5a019de7666578bb60ab08cc80ee92897ba7966
lert <tplermit@gmail.com>
2022-12-04 20:43:05 -0400

    middleware

--- middleware.ts
+++ middleware.ts
@@ -8,11 +8,13 @@ export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/checkout')) {
     const token = request.cookies.get('token')?.value || ''
     console.log('::::::startsWith checkout',token);
-    // const returnTo = request.nextUrl.pathname
-    const hasValidToken = await jwt.isValidToken(token)
-    console.log({hasValidToken});
-    return NextResponse.next()
-    // return NextResponse.redirect(new URL(`/auth/login?p=${returnTo}`,request.url))
+    const returnTo = request.nextUrl.pathname
+    // const hasValidToken = await jwt.isValidToken(token)
+    // console.log({hasValidToken});
+    if(token === '')
+      return NextResponse.redirect(new URL(`/auth/login?p=${returnTo}`,request.url))
+    else
+      return NextResponse.next()
     
   }
 
